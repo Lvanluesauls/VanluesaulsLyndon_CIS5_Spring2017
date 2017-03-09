38156b9f04a1e8f2ca76632a86436d10d995c961
rcc <rcc>
2017-03-09 12:23:57 -0800

    Unfinished chap 4 problem

---
+++ lab/Gaddis_8thEd_Chap4_MinMax/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Lyndon Vanluesauls
+ * Created on March 09, 2017, 12:13 AM
+ * Purpose:  Enter two numbers and display which number is smaller and larger
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    int num1,num2; //Variable for two numbers
+    
+    //Input data
+    cout<<"Enter two numbers in integer form: ";
+    cin>>num1>>num2;
+    
+    //Map inputs to outputs or process the data
+    
+                
+    //Output the transformed data
+    
+    //Exit stage right!
+    return 0;
+}
+
